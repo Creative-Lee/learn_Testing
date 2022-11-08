4f940f3b1b2d7c767aa3f42681c99b00327683c4
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-09 00:40:53 +0900

    feat: test용 함수 작성

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function test1() {
+  return "123";
+}
+function test2() {
+  return 123;
+}
+function test3() {
+  return { 123: 123 };
+}
+function test4() {
+  return [123];
+}
+function test5() {
+  return new Promise();
+}
+async function test6() {
+  return "123";
+}
