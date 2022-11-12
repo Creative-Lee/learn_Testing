b9b07505c884b8f1cb51e94353afe3674111ded2
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-12 12:03:15 +0900

    test: toEqual undefined 테스트 완료

--- __tests__/MatcherTest.js
+++ __tests__/MatcherTest.js
@@ -12,7 +12,7 @@ describe("toBe 테스트", () => {
   });
 });
 
-describe.only("toEqual 테스트", () => {
+describe("toEqual 테스트", () => {
   test("원시형 비교", () => {
     const output = "123";
 
@@ -23,5 +23,19 @@ describe.only("toEqual 테스트", () => {
     const output = [1, 2, 3];
 
     expect(output).toEqual([1, 2, 3]); // test passed!
+  });
+});
+
+describe.only("toEqual undefined 테스트", () => {
+  test("참조형 비교 - 객체", () => {
+    const output = { a: 1, b: 2 };
+
+    expect(output).toEqual({ a: 1, b: 2, c: undefined }); // test passed!!!!????
+  });
+
+  test("참조형 비교 - 배열", () => {
+    const output = [1, 2, 3];
+
+    expect(output).toEqual([1, 2, 3, undefined]); // test passed!!!!!???
   });
 });
