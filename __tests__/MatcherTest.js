8518e90d33f46a8b352373f4fa4f04292e6d0f13
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-12 12:36:05 +0900

    test: toBeTruthy / toBeFalsy 테스트 완료

--- __tests__/MatcherTest.js
+++ __tests__/MatcherTest.js
@@ -1,60 +1,60 @@
 describe("toBe 테스트", () => {
   test("원시형 비교", () => {
-    const output = "123";
+    const result = "123";
 
-    expect(output).toBe("123"); // test passed!
+    expect(result).toBe("123"); // test passed!
   });
 
   test("참조형 비교", () => {
-    const output = [1, 2, 3];
+    const result = [1, 2, 3];
 
-    expect(output).toBe([1, 2, 3]); // test failed!
+    expect(result).toBe([1, 2, 3]); // test failed!
   });
 });
 
 describe("toEqual 테스트", () => {
   test("원시형 비교", () => {
-    const output = "123";
+    const result = "123";
 
-    expect(output).toEqual("123"); // test passed!
+    expect(result).toEqual("123"); // test passed!
   });
 
   test("참조형 비교", () => {
-    const output = [1, 2, 3];
+    const result = [1, 2, 3];
 
-    expect(output).toEqual([1, 2, 3]); // test passed!
+    expect(result).toEqual([1, 2, 3]); // test passed!
   });
 });
 
 describe("toEqual undefined 테스트", () => {
   test("참조형 비교 - 객체", () => {
-    const output = { a: 1, b: 2 };
+    const result = { a: 1, b: 2 };
 
-    expect(output).toEqual({ a: 1, b: 2, c: undefined }); // test passed!!!!????
+    expect(result).toEqual({ a: 1, b: 2, c: undefined }); // test passed!!!!????
   });
 
   test("참조형 비교 - 배열", () => {
-    const output = [1, 2, 3];
+    const result = [1, 2, 3];
 
-    expect(output).toEqual([1, 2, 3, undefined]); // test passed!!!!!???
+    expect(result).toEqual([1, 2, 3, undefined]); // test passed!!!!!???
   });
 });
 
 describe("toStrictEqual undefined 테스트", () => {
   test("참조형 비교 - 객체", () => {
-    const output = { a: 1, b: 2 };
+    const result = { a: 1, b: 2 };
 
-    expect(output).toStrictEqual({ a: 1, b: 2, c: undefined }); // test failed ^^
+    expect(result).toStrictEqual({ a: 1, b: 2, c: undefined }); // test failed ^^
   });
 
   test("참조형 비교 - 배열", () => {
-    const output = [1, 2, 3];
+    const result = [1, 2, 3];
 
-    expect(output).toStrictEqual([1, 2, 3, undefined]); //  test failed ^^
+    expect(result).toStrictEqual([1, 2, 3, undefined]); //  test failed ^^
   });
 });
 
-describe.only("toEqual vs toStrictEqual 인스턴스 테스트", () => {
+describe("toEqual vs toStrictEqual 인스턴스 테스트", () => {
   class Test {
     constructor(test) {
       this.test = test;
@@ -73,5 +73,19 @@ describe.only("toEqual vs toStrictEqual 인스턴스 테스트", () => {
     const normal = { test: 1 };
 
     expect(normal).toStrictEqual(instance); // test failed !!
+  });
+});
+
+describe.only("toBeTruthy / toBeFalsy 테스트", () => {
+  test("toBeTruthy 테스트", () => {
+    const result = "0";
+
+    expect(result).toBeTruthy(); // test passed !!
+  });
+
+  test("toBeFalsy 테스트", () => {
+    const result = 0;
+
+    expect(result).toBeFalsy(); // test passed !!
   });
 });
