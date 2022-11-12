50cd00012dffafa7f56b6a96891e190599d313d1
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-12 11:50:11 +0900

    test: toEqual 테스트 완료

--- __tests__/MatcherTest.js
+++ __tests__/MatcherTest.js
@@ -11,3 +11,17 @@ describe("toBe 테스트", () => {
     expect(output).toBe([1, 2, 3]); // test failed!
   });
 });
+
+describe.only("toEqual 테스트", () => {
+  test("원시형 비교", () => {
+    const output = "123";
+
+    expect(output).toEqual("123"); // test passed!
+  });
+
+  test("참조형 비교", () => {
+    const output = [1, 2, 3];
+
+    expect(output).toEqual([1, 2, 3]); // test passed!
+  });
+});
